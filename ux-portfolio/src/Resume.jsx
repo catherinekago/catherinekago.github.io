52a5fc01a3cd07ee344a49697bf6bce3cb8bdfaa
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2023-03-15 17:37:05 +0100

    Add routing

---
+++ ux-portfolio/src/Resume.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color:'black'
+};
+
+class Resume extends React.Component {
+    
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>Resume</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default Resume;
