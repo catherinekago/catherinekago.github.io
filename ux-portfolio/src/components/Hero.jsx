68811335ce618770b22d6295a18d902bdda58379
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2023-03-16 18:26:12 +0100

    Add custom button component to hero, adjust responsive design

--- ux-portfolio/src/components/Hero.jsx
+++ ux-portfolio/src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
       <div id="hero-text-container">
         <h1>Hi, I'm Kathrin.</h1>
         <p  class='title-large'>Designer and developer with a passion for inclusive and assistive technologies. Currently, I am facilitating climate action at <b>ClimatePartner</b>.</p>
+        <button class="button-large">View my work</button>
         </div>
     </div>
     </>
