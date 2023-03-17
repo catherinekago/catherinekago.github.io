4bad53dd6edaaa0a05f0b45b868873f97e2b5fa0
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2023-03-17 17:35:39 +0100

    Add additional project dummies with responsive design

--- ux-portfolio/src/pages/Portfolio.jsx
+++ ux-portfolio/src/pages/Portfolio.jsx
@@ -7,6 +7,7 @@ import './Portfolio.scss'
 const Portfolio = () => {
 
     let featuredProjects = require('../content.json').portfolio.featuredProjects;
+    let additionalProjects = require('../content.json').portfolio.additionalProjects;
 
     return (
         <>
@@ -17,8 +18,15 @@ const Portfolio = () => {
                     <ProjectTeaser teaserType="project-featured" textPosition={index % 2 === 0 ? "text-left" : "text-right"} title={project.title} description={project.description} button={project.button} projectType={project.projectType} chips={project.chips} />
                 ))}
 
-            <Section text="Additional projects" />
+                <Section text="Additional projects" />
+                <div className="additional-teaser-container">
+                        <div class="project-pair-container">
+                                <ProjectTeaser teaserType="project-additional" textPosition={"text-right"} title={additionalProjects.pair1[0].title} description={additionalProjects.pair1[0].description} button={additionalProjects.pair1[0].button} projectType={additionalProjects.pair1[0].projectType} chips={additionalProjects.pair1[0].chips} />
+                              <ProjectTeaser teaserType="project-additional" textPosition={"text-right"} title={additionalProjects.pair1[1].title} description={additionalProjects.pair1[1].description} button={additionalProjects.pair1[1].button} projectType={additionalProjects.pair1[1].projectType} chips={additionalProjects.pair1[1].chips} />
 
+                        </div>
+                
+                </div>
             </div>
 
         </>
