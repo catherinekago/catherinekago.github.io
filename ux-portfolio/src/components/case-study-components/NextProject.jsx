b2575cad9d7d7b44951d195b1b7df1103d0dbe69
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2023-08-04 17:54:49 +0200

    Adjust next project component

--- ux-portfolio/src/components/case-study-components/NextProject.jsx
+++ ux-portfolio/src/components/case-study-components/NextProject.jsx
@@ -4,9 +4,9 @@ import "./NextProject.scss";
 
 const NextProject = (props) => {
   return (
-    <div class="mcontainer-white next-project-container">
-      <p class="text-dark next-text">
-        Next: <span class="text-primary text-title">{props.project}</span>
+    <div class="container-dark next-project-container">
+      <p class="text-light next-text">
+        Next: <span class="text-light text-title">{props.project}</span>
       </p>
       <Link class="button-medium" to={props.link}>
         <button>{props.button}</button>
