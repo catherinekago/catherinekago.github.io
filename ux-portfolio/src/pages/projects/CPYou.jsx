40056b15c920d0824d63bbeca87eb3db581b56ff
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2023-08-31 23:26:46 +0200

    Finish CP YOU page

--- ux-portfolio/src/pages/projects/CPYou.jsx
+++ ux-portfolio/src/pages/projects/CPYou.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./../../style/style.scss";
+import "./../../components/case-study-components/CarouselProject.scss";
 
 import CPYouHero from "../../assets/images/CPYou/you_hero.svg";
 import LoadingScreen from "../LoadingScreen";
 import you_personas from "../../assets/images/CPYou/you_personas.png";
 import you_brand from "../../assets/images/CPYou/you_brand.png";
+import you_feature_1 from "../../assets/images/CPYou/you_feature_1.png";
+import you_feature_2 from "../../assets/images/CPYou/you_feature_2.png";
+import you_feature_3 from "../../assets/images/CPYou/you_feature_3.png";
+import you_feature_4 from "../../assets/images/CPYou/you_feature_4.png";
+import you_potential_1 from "../../assets/images/CPYou/you_potential_1.png";
+import you_potential_2 from "../../assets/images/CPYou/you_potential_2.png";
+import you_potential_3 from "../../assets/images/CPYou/you_potential_3.png";
 
 import ProjectHero from "../../components/project-components/ProjectHero";
 import CaseStudyDescription from "../../components/case-study-components/CaseStudyDescription";
 import CaseStudyOverview from "../../components/case-study-components/CaseStudyOverview";
 import ContentBlock from "../../components/case-study-components/ContentBlock";
 import CaseStudyContent from "../../components/case-study-components/CaseStudyContent";
+import CarouselProject from "../../components/case-study-components/CarouselProject";
 
 const CPYou = () => {
   const size = useWindowSize();
@@ -127,6 +136,123 @@ const CPYou = () => {
       }
     />
   );
+  const featureContent = [
+    {
+      title: <p class="text-l text-title primaryMain">Consider time</p>,
+      imgSource: you_feature_1,
+      alt: "A mockup showing a screen of our calculator where the user can choose between a fast calculation that takes 3min to complete, and a detailed calculation that takes 10min to complete.",
+    },
+    {
+      title: <p class="text-l text-title primaryMain">Easy estimations</p>,
+      imgSource: you_feature_2,
+      alt: "A mockup showing a screen of our calculator where the user can enter information about their diet through estimations.",
+    },
+    {
+      title: <p class="text-l text-title primaryMain">Show impact</p>,
+      imgSource: you_feature_3,
+      alt: "A mockup showing a screen of our calculator where the user sees an overview of all their emissions and what category they belong to.",
+    },
+    {
+      title: <p class="text-l text-title primaryMain">Educate</p>,
+      imgSource: you_feature_4,
+      alt: "A mockup showing a screen of our calculator where the user learns about the difference between their own carbon footprint and the global carbon footprint.",
+    },
+  ];
+
+  const featureItems = featureContent.map((item, index) => (
+    <div class={"project-carousel-item-container you-yellow"}>
+      <div className="carousel-project-header">
+        <div className="carousel-project-item-number you-red">
+          <p class="text-l text-title text-white">{index + 1}</p>
+        </div>
+        {item.title}
+      </div>
+      <img class="carousel-project-image" src={item.imgSource} alt={item.alt} />
+    </div>
+  ));
+
+  const responsive = {
+    0: { items: 1 },
+    600: { items: 2 },
+    900: { items: 3 },
+    1200: { items: 4 },
+  };
+
+  const block_3 = (
+    <ContentBlock
+      size={size}
+      alignment="vertical"
+      text={
+        <p class="text-l primaryMain">
+          Based on our persona insights we defined{" "}
+          <span class="text-l text-title">four key features</span> our carbon
+          footprint calculator had to incorporate:
+        </p>
+      }
+      content={<CarouselProject responsive={responsive} items={featureItems} />}
+    />
+  );
+
+  const potentialContent = [
+    {
+      title: (
+        <p class="text-l text-title primaryMain centered">
+          Individual <br /> reduction plans
+        </p>
+      ),
+      imgSource: you_potential_1,
+      alt: "A wireframe....",
+    },
+    {
+      title: (
+        <p class="text-l text-title primaryMain centered">
+          Handle <br /> unavoidable emissions
+        </p>
+      ),
+      imgSource: you_potential_2,
+      alt: "A wireframe....",
+    },
+    {
+      title: (
+        <p class="text-l text-title primaryMain centered">
+          Track <br /> your progress
+        </p>
+      ),
+      imgSource: you_potential_3,
+      alt: "A wireframe....",
+    },
+  ];
+
+  const potentialItems = potentialContent.map((item, index) => (
+    <div class={"project-carousel-item-container"}>
+      <div className="carousel-project-header">
+        <div className="carousel-project-item-number you-red">
+          <p class="text-l text-title text-white">{index + 1}</p>
+        </div>
+        {item.title}
+      </div>
+      <img class="carousel-project-image" src={item.imgSource} alt={item.alt} />
+    </div>
+  ));
+
+  const block_4 = (
+    <ContentBlock
+      size={size}
+      alignment="vertical"
+      text={
+        <p class="text-l primaryMain">
+          Through the user tests conducted on our digital prototypes, we
+          revealed{" "}
+          <span class="text-l text-title">additional opportunities</span> to
+          empower our target audience to take climate action in ways that align
+          with their personal preferences:
+        </p>
+      }
+      content={
+        <CarouselProject responsive={responsive} items={potentialItems} />
+      }
+    />
+  );
 
   return (
     <>
@@ -154,6 +280,18 @@ const CPYou = () => {
         color="white"
         section="Establish the CP YOU brand"
         content={[block_2]}
+      />
+
+      <CaseStudyContent
+        color="white"
+        section="Define key features"
+        content={[block_3]}
+      />
+
+      <CaseStudyContent
+        color="white"
+        section="Reveal the potential"
+        content={[block_4]}
       />
     </>
   );
